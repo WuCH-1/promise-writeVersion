3f941f5afd637d1c093a876c81ea5d75e0b304cc
吴琛 <wuchen@xhd.cn>
2022-07-18 17:28:44 +0800

    手写call,apply,new

--- mybind.js
+++ mybind.js
@@ -11,3 +11,47 @@ Function.prototype.mybind = function (context) {
         return _this.apply(context, args.concat(...arguments))
     }
 }
+
+Function.prototype.mycall = function(context) {
+    if(typeof this !=='function') {
+        throw new TypeError('error')
+    }
+    const obj = context || window
+    obj.fn = this
+    const args = [...arguments].slice(1)
+    var result = obj.fn(...args)
+    delete obj.fn
+    return result
+}
+Function.prototype.myapply = function(context = window,arr){
+    if(typeof this !=='function') {
+        throw new TypeError('error')
+    }
+    context.fn = this
+    var result = context.fn(...arr)
+    delete context.fn
+    return result
+}
+
+function myNew(Fn,...args) {
+    if(typeof Fn !=='function') {
+        throw new TypeError('errpr')
+    }
+    let obj = Object.create(Fn.prototype)
+    let res = Fn.apply(obj,args)
+    return res instanceof Object?res:obj
+}
+
+a = 1, b = 1
+obj = { a: 5 }
+function app(b) {
+   return this.a + b
+}
+app2 = app.mybind(obj)
+
+var Fun = function(a,b){
+    this.a = a
+    this.b = b
+    return 1
+}
+console.log(myNew(Fun,1,2))
